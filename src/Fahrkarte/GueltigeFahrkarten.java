9ed2611de37fa85227aedf3392c56dfb4da3c356
stevo25-bit <steven.wesner@web.de>
2025-09-06 20:09:41 +0200

    Neuee Interface eingefügt

---
+++ src/Fahrkarte/GueltigeFahrkarten.java
@@ -0,0 +1,6 @@
+package Fahrkarte;
+
+public interface GueltigeFahrkarten {
+    // Bitte definieren Sie diese methode in den weiteren Klassen
+    public Fahrkarte[] pruefen(Fahrkarte[] fahrkarten);
+}
